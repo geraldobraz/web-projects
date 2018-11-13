966bd4b1d9321567519d9c494d6a9b1555e39f08
geraldobraz <gban50@gmail.com>
2018-11-12 21:29:16 -0300

    Uploading the variables code

---
+++ JavaScript/variables.js
@@ -0,0 +1,27 @@
+ // Strings
+ var text1 = "Hello";
+ var text2 = "World!";
+
+ // Float
+var floatNumber = 3.14;
+
+//  Int 
+var intNumber = -2;
+
+// Boolean
+var boolState = true;
+
+// Debug
+console.log("Hello World!")
+
+// Alert
+alert("Hello World!")
+
+// Write on Html Page
+document.write(text1+ " "+text2);
+document.write('<br/><br/>');
+document.write(text2);
+
+// Read data from page
+var text3 = prompt("Insert a name: ");
+document.write(text3);
